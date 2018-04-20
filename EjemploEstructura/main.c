1b4e4117ea1ed8a649c80ccc32005c0483caf100
upszot <upszot@gmail.com>
2018-04-20 17:31:20 -0300

    primer estructura

---
+++ EjemploEstructura/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int legajo;
+    int nota1;
+    int nota2;
+    float promedio;
+    char nombre[50];
+    int estado;
+}T_Alumno;
+
+int main()
+{
+
+
+
+    return 0;
+}
+
